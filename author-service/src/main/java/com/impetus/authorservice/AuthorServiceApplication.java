72ac28dd2d2e5ed39ddca58fa51972b3edec5da2
Ashish Rawat <31473799+ashishasr@users.noreply.github.com>
2020-08-05 16:28:15 +0530

    added author service

---
+++ author-service/src/main/java/com/impetus/authorservice/AuthorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.impetus.authorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorServiceApplication.class, args);
+	}
+
+}
