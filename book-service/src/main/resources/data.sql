c6fd1dda8991e46a9098aa9e1f245c1f42cc444b
ashishasr <ashishrawat06@gmail.com>
2020-08-17 14:36:21 +0530

    .\data.sql

---
+++ book-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+ insert into book (id, auther_id, name, price) values(1, 1, 'spark', 500);
+ insert into book (id, auther_id, name, price) values(2, 2, 'Core java', 400);
+ insert into book (id, auther_id, name, price) values(3, 2, 'Advance Java', 250);
