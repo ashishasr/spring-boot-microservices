166239b1761269595711136256ae773758df68b7
ashishasr <ashishrawat06@gmail.com>
2020-08-17 14:35:20 +0530

    .\data.sql

---
+++ genre-service/src/main/resources/data.sql
@@ -0,0 +1,3 @@
+ insert into genre (id, book_id, category) values (1, 1, 'Programming');
+ insert into genre (id, book_id, category) values (2, 2, 'Advance Programming');
+ insert into genre (id, book_id, category) values (3, 3, 'Programming');
